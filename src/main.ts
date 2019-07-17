142b0348e31f889acda631452f55e826ae15c530
CursedFlames <18627001+CursedFlames@users.noreply.github.com>
2019-07-17 14:05:15 +1200

    more worldgen stuff idk

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import * as THREE from "three";
 import OpenSimplexNoise from 'open-simplex-noise';
 import * as FractalNoise from "fractal-noise";
+import { Board } from "src/worldgen";
 
 // OrbitControls.js expects a global THREE object
 (window as any).THREE = THREE;
@@ -20,41 +21,46 @@ document.body.appendChild(renderer.domElement);
 
 const controls = new (<any>THREE).OrbitControls(camera, renderer.domElement);
 
-// var geometry = new THREE.BoxGeometry(1, 1, 1);
-var material = new THREE.MeshBasicMaterial({ color: 0x00ff00});
-// var cube = new THREE.Mesh(geometry, material);
-// scene.add(cube);
-
-const noise = new OpenSimplexNoise(Date.now());
-
 var light = new THREE.PointLight(0xffffff);
 light.position.set(10, 10, 10);
 scene.add(light);
 
-var height: number[][] = <any>FractalNoise.makeRectangle(200, 200, (x,y)=>noise.noise2D(x,y), {octaves: 8, persistence: 0.5, frequency: 0.05})
-console.log(height);
+var ambientLight = new THREE.AmbientLight(0x666666);
+scene.add(ambientLight);
 
-var material2 = new THREE.MeshStandardMaterial(
-        {vertexColors: THREE.VertexColors, side: THREE.DoubleSide, metalness: 0, roughness: 1});
+const board = new Board(200, 200);
+
+var material = new THREE.MeshStandardMaterial(
+		{vertexColors: THREE.VertexColors, side: THREE.DoubleSide, metalness: 0, roughness: 1});
 var planeGeo = new THREE.PlaneGeometry(10, 10, 199, 199);
+
 for (let i = 0; i < planeGeo.vertices.length; i++) {
-    let v = planeGeo.vertices[i];
-    v.z = height[i%200][Math.floor(i/200)];
-    planeGeo.colors[i] = new THREE.Color(0xCCCCCC);
+	let v = planeGeo.vertices[i];
+	v.z = board.heightmap[i%200][Math.floor(i/200)];
+	// planeGeo.colors[i] = board.colormap[i%200][Math.floor(i/200)]
 }
-var plane = new THREE.Mesh(planeGeo, material2);
+for (let i = 0; i < planeGeo.faces.length; i++) {
+	let face = planeGeo.faces[i];
+
+	face.vertexColors = [board.colormap[face.a%200][Math.floor(face.a/200)],
+	                     board.colormap[face.b%200][Math.floor(face.b/200)],
+	                     board.colormap[face.c%200][Math.floor(face.c/200)]];
+}
+planeGeo.colorsNeedUpdate = true;
+var plane = new THREE.Mesh(planeGeo, material);
 scene.add(plane);
 
 var water = new THREE.PlaneGeometry(10, 10, 1, 1);
-var waterMaterial = new THREE.MeshStandardMaterial({color: new THREE.Color(0x3333FF), side: THREE.DoubleSide});
+var waterMaterial = new THREE.MeshStandardMaterial(
+        {color: new THREE.Color(0x3333FF), side: THREE.DoubleSide, metalness: 0.2, roughness: 0.6});
 scene.add(new THREE.Mesh(water, waterMaterial));
 
 camera.position.z = 5;
 
 function draw(timestamp: number) {
-    requestAnimationFrame(draw);
-    renderer.render(scene, camera);
-    // cube.rotation.x += 0.01;
-    // cube.rotation.y += 0.01;
+	requestAnimationFrame(draw);
+	renderer.render(scene, camera);
+	// cube.rotation.x += 0.01;
+	// cube.rotation.y += 0.01;
 }
 requestAnimationFrame(draw);
--- src/worldgen.ts
+++ src/worldgen.ts
@@ -1,21 +1,31 @@
 import { Color } from "three";
+import OpenSimplexNoise from 'open-simplex-noise';
+import * as FractalNoise from "fractal-noise";
+
+const noise = new OpenSimplexNoise(Date.now());
 
 interface WorldgenData {
-    width: number;
-    height: number;
-    heightmap: number[][];
-    colormap: Color[][];
+	width: number;
+	height: number;
+	heightmap: number[][];
+	colormap: Color[][];
 }
 
-class Board {
-    heightmap: number[][];
-    constructor(public width: number, public height: number) {
-        for (let y = 0; y < height; y++) {
-            let row = [];
-            for (let x = 0; x < width; x++) {
-                row.push(0);
-            }
-            this.heightmap.push(row);
-        }
-    }
+const snow = new Color(0xEEEEEE);
+const grass = new Color(0x22AA11);
+
+export class Board {
+	heightmap: number[][] = [];
+	colormap: Color[][] = [];
+	constructor(public width: number, public height: number) {
+		this.heightmap = <any>FractalNoise.makeRectangle(width, height, (x,y)=>noise.noise2D(x,y),
+				{octaves: 8, persistence: 0.5, frequency: 0.05});
+		for (let x = 0; x < width; x++) {
+			let col = [];
+			for (let y = 0; y < height; y++) {
+				col.push(grass.clone().lerpHSL(snow, Math.min(1, Math.pow(Math.max(0, this.heightmap[x][y]), 1.5))));
+			}
+			this.colormap.push(col);
+		}
+	}
 }
